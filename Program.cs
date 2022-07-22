503f783aee9f71725633250dbe13cfc79a1fd153
ThiagoAvelino1990 <thiagoavelinoalves@hotmail.com>
2022-07-21 22:48:29 -0300

    Criação dos métodos CarcularImpostoDeRenda e CalcularINSS

--- Program.cs
+++ Program.cs
@@ -1,35 +1,63 @@
-﻿Console.WriteLine("Valor do salário:");
+﻿//Método para calcular imposto de renda
+double CalcularImpostoDeRenda (double salario)
+{
+    if(salario > 1903.99 && salario <= 2829.65)
+    {
+        double aliquota = 0.925;
+        salario = (salario * aliquota) + 142.80;
+        return salario;
+    }
+    if (salario >= 2826.65 && salario <= 3751.05)
+    {
+        double aliquota = 0.85;
+        salario = (salario * aliquota) + 354.80;
+        return salario;
+    }
+
+    if (salario >= 3751.05 && salario <= 4664.68)
+    {
+        double aliquota = 0.775;
+        salario = (salario * aliquota) + 636.13;
+        return salario;
+    }
+
+    if (salario >= 4664.68)
+    {
+        double aliquota = 0.725;
+        salario = (salario * aliquota) + 869.36;
+        return salario;
+    }
+
+    return salario;
+
+}
+
+//Método para calcular INSS
+double CalcularINSS(double salario)
+{
+    double descINSS = 0;
+    if (salario <= 1830.29)
+    {
+        descINSS = ((salario * 8) / 100);
+        return descINSS;
+    }
+    if(salario >= 1830.30 && salario <= 3050.52)
+    {
+        descINSS = ((salario * 9) / 100);
+        return descINSS;
+    }
+    if(salario >= 3050.53 && salario <= 6101.06)
+    {
+        descINSS = ((salario * 11) / 100);
+        return descINSS;
+    }
+    return descINSS;
+}
+
+Console.WriteLine("Valor do salário:");
 var salario = Convert.ToDouble(Console.ReadLine());
 
-double aliquota = 0;
-
-if (salario <= 1903.98)
-{
-    salario = salarioliquido;
-}
-
-if (salario > 1903.98 && salario <= 2826.65)
-{
-    aliquota = 0.925;
-    salarioliquido = (salario * aliquota) + 142.80; 
-}
-
-if (salario >= 2826.65 &&  salario <= 3751.05 )
-{
-    aliquota = 0.85;
-    salarioliquido = (salario * aliquota) + 354.80; 
-}
-
-if (salario >= 3751.05 && salario <= 4664.68)
-{
-    aliquota = 0.775;
-    salarioliquido = (salario * aliquota) + 636.13;
-}
-
-if (salario >= 4664.68)
-{
-    aliquota = 0.725;
-    salarioliquido = (salario * aliquota) + 869.36; 
-}
-
-Console.WriteLine($"Salário liquido de: {Math.Round(salarioliquido, 2)}");
+Console.WriteLine($"Salario com desconto do imposto de renda: {CalcularImpostoDeRenda(salario)}");
+Console.WriteLine($"Desconto INSS: {CalcularINSS(salario)}");
+//Exemplo de interpolação
+Console.WriteLine($"Valor do salário com descontos: {Math.Round(CalcularImpostoDeRenda(salario) - CalcularINSS(salario),2)}");
