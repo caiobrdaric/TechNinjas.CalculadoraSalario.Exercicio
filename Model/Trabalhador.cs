e0b3c2e09b0db45ad29f0291b66c7c6fb9c0a2e1
Caio Brdaric <caiobrdaric@gmail.com>
2022-08-03 08:30:59 -0300

    Adicionado classe e propriedades

---
+++ Model/Trabalhador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechNinjas.CalculadoraSalario.Exercicio.Model
+{
+    public class Trabalhador
+    {
+        public char nome { get; set; }
+        public double salarioBruto { get; set; }
+        public double salarioLiquido { get; set; }
+    }
+}
